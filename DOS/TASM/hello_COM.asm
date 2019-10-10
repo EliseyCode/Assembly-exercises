bacb1b43500eda1b709d5629722e2d63726c71d2
Elisey <enovikow6@gmail.com>
2019-10-10 03:20:56 +0300

    Update hello_COM.asm
    edit code format

--- DOS/TASM/hello_COM.asm
+++ DOS/TASM/hello_COM.asm
@@ -1,13 +1,9 @@
-.model tiny
-.code 
-org 100h
-
-start:
-	mov ah, 			9
-	lea dx, 			message
-	int 				21h
+	.model	tiny
+	.code 
+	org 	100h
+start:	mov 	ah, 9
+	mov 	dx, offset message
+	int 	21h
 	ret
-
-	message db			'Hello world!$'
-
-end start
+message db	"Hello world!", 0Dh, 0Ah, '$'
+	end 	start
