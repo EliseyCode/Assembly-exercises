bed8b1d4f4fb3e00482ca1beb66005babec77e06
Elisey <enovikow6@gmail.com>
2019-10-10 03:17:22 +0300

    Update hello_EXE.asm
    edit code format

--- DOS/TASM/hello_EXE.asm
+++ DOS/TASM/hello_EXE.asm
@@ -2,12 +2,12 @@
 		.stack 	100h
 		.code
 start:	mov		ax, DGROUP
-		mov		ds, ax
-		mov		dx, offset message
-		mov		ah, 9
-		int		21h
-		mov		ax, 4C00h
-		int		21h
+		mov	ds, ax
+		mov	dx, offset message
+		mov	ah, 9
+		int	21h
+		mov	ax, 4C00h
+		int	21h
 		.data
 message db		"Hello World!", 0Dh, 0Ah, '$'
 		end 	start
